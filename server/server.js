3b75bd060b7903a545b5e4ce3fadccdcdf447fb1
Irfan Dayan <engr.irfandayan@gmail.com>
2023-01-31 12:29:50 +0500

    setup server

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ import helmet from "helmet"; // set some security
 import morgan from "morgan";
 
 // Configurations
-const __filename = fileURLToPath(import.meta.url); // Use this incase of type as module
+const __filename = fileURLToPath(import.meta.url); // use this in case of type as module
 const __dirname = path.dirname(__filename);
 dotenv.config();
 const app = express();
@@ -20,4 +20,25 @@ app.use(helmet());
 app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(morgan("common"));
 app.use(cors());
-app.use("/assets", express.static(path.join(__dirname, "public/assets")));
+app.use("/assets", express.static(path.join(__dirname, "public/assets"))); // set static files folder
+
+// File storage
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "public/assets");
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  },
+});
+const upload = multer({ storage });
+
+// Connect db & run server
+const PORT = process.env.PORT || 6001;
+mongoose.set("strictQuery", true);
+mongoose
+  .connect(process.env.MONGO_URL, {})
+  .then(() => {
+    app.listen(PORT, () => console.log(`Server is running on port: ${PORT}`));
+  })
+  .catch((error) => console.log(`${error} - did not connect`));
