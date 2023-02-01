d3b45ea5e95a1e300a4917e0ee56a503def0cd3a
Irfan Dayan <engr.irfandayan@gmail.com>
2023-02-01 15:42:05 +0500

    add folder architecture & react router setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,20 @@
+import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
+import HomePage from "scenses/homePage";
+import LoginPage from "scenses/loginPage";
+import ProfilePage from "scenses/profilePage";
+
 function App() {
-  return <div className="app"></div>;
+  return (
+    <div className="app">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LoginPage />} />
+          <Route path="home" element={<HomePage />} />
+          <Route path="profile/:usreId" element={<ProfilePage />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
