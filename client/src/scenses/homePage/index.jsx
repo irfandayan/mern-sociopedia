a59cfee56d4b8101f345a0b197c13179cc23ded2
Irfan Dayan <engr.irfandayan@gmail.com>
2023-02-02 16:18:33 +0500

    add desktop & mobile nav bar

--- client/src/scenses/homePage/index.jsx
+++ client/src/scenses/homePage/index.jsx
@@ -1,5 +1,12 @@
+import { Box } from "@mui/material";
+import Navbar from "scenses/navbar";
+
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <Box>
+      <Navbar />
+    </Box>
+  );
 };
 
 export default HomePage;
