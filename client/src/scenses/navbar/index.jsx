c524458678b2e3bf0faf6b6bce5f63d0c938a36a
Irfan Dayan <engr.irfandayan@gmail.com>
2023-02-03 09:42:01 +0500

    add register & login form

--- client/src/scenses/navbar/index.jsx
+++ client/src/scenses/navbar/index.jsx
@@ -39,8 +39,8 @@ const Navbar = () => {
   const primaryLight = theme.palette.primary.light;
   const alt = theme.palette.background.alt;
 
-  // const fullName = `${user.firstName} ${user.lastName}`;
-  const fullName = "Irfan Dayan";
+  const fullName = `${user.firstName} ${user.lastName}`;
+  // const fullName = "Irfan Dayan";
 
   return (
     <FlexBetween padding="1rem 6%" backgroundColor={alt}>
