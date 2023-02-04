59c78021a1d4ae10ed9f4e64e9364cb66eb8397b
Irfan Dayan <engr.irfandayan@gmail.com>
2023-02-04 12:01:46 +0500

    Add page title and provided demo email & password

--- client/src/scenes/loginPage/index.jsx
+++ client/src/scenes/loginPage/index.jsx
@@ -1,5 +1,6 @@
 import { Box, Typography, useTheme, useMediaQuery } from "@mui/material";
 import Form from "./Form";
+import InfoOutlinedIcon from "@mui/icons-material/InfoOutlined";
 
 const LoginPage = () => {
   const theme = useTheme();
@@ -27,6 +28,32 @@ const LoginPage = () => {
         <Typography fontWeight="500" variant="h5" sx={{ mb: "1.5rem" }}>
           Welcome to Sociopedia, the Social Media for Sociopaths!
         </Typography>
+
+        <Box
+          sx={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            color: "#00D5FA",
+            mb: "20px",
+          }}
+        >
+          <InfoOutlinedIcon />
+          <Typography
+            sx={{
+              fontWeight: "bold",
+              fontSize: "14px",
+              textAlign: "center",
+              justifyContent: "center",
+              alignItems: "center",
+              ml: "2px",
+              mt: "2px",
+            }}
+          >
+            Use email: someguy@gmail.com & password: test
+          </Typography>
+        </Box>
+
         <Form />
       </Box>
     </Box>
